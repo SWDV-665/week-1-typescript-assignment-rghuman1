e796b711a37c680f02c7617f5642ed864bbf6c43
rghuman1 <132854199+rghuman1@users.noreply.github.com>
2023-10-25 13:41:33 -0400

    Update grocery.ts

--- grocery.ts
+++ grocery.ts
@@ -1,7 +1,6 @@
 class Grocery {
     name: string;
     quantity: number;
-    // You can add more attributes if needed, like price, category, etc.
   
     constructor(name: string, quantity: number) {
       this.name = name;
@@ -9,13 +8,11 @@ class Grocery {
     }
   }
   
-  // Creating an array of grocery items
   const groceries: Grocery[] = [
     new Grocery("Milk", 3),
     new Grocery("Bread", 6),
     new Grocery("Eggs", 11),
   ];
-  
-  // Export the groceries array so it can be used in other files
+
   export { groceries };
   
